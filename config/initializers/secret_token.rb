f9622ed1061f1569aa81976dbe41223da1559dce
Steve Lair <steve.lair@gmx.de>
2012-08-24 13:11:21 +0200

    Nackt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scaffoldtest::Application.config.secret_token = '1c2936c44d39e9e62ca7e8922cd29973a101f93b75dff310988abb1f2ab4d0b62bcbbd11778c9bf2fb306c10e181011ffa514b27f908a7ffcc2ba432b903cd74'
